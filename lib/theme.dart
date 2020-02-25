bc660aadfc86727824a2d7ff31d58e56af4cad32
Alex <ahsmb22@gmail.com>
2020-02-25 22:13:51 +1300

    fonts working

--- lib/theme.dart
+++ lib/theme.dart
@@ -2,36 +2,42 @@ import 'package:flutter/material.dart';
 
 //The purpose here is COLOURS, FONTS, TEXT STYLES
 ThemeData basicTheme() {
+  String fontFamily = 'Playfair';
+
   TextTheme _basicTextTheme(TextTheme base) {
     return base.copyWith(
         headline: base.headline.copyWith(
-          fontFamily: 'Georgia',
+          fontFamily: fontFamily,
           fontSize: 22.0,
           color: Colors.white,
         ),
         title: base.title.copyWith(
-          fontFamily: 'Georgia',
-          fontSize: 30.0,
-          color: Colors.white70,
-          fontStyle: FontStyle.italic
-        ),
+            fontFamily: fontFamily,
+            fontSize: 30.0,
+            color: Colors.white70,
+            fontStyle: FontStyle.italic),
         display1: base.headline.copyWith(
-          fontFamily: 'Georgia',
+          fontFamily: fontFamily,
           fontSize: 24.0,
           color: Colors.white30,
         ),
         display2: base.headline.copyWith(
-          fontFamily: 'Georgia',
+          fontFamily: fontFamily,
           fontSize: 22.0,
           color: Colors.white,
         ),
         caption: base.caption.copyWith(
           color: Colors.white,
         ),
-        body1: base.body1.copyWith(color: Colors.white),
-        body2: base.body2.copyWith(color: Colors.yellow)
-        );
+        body1: base.body1.copyWith(
+            color: Colors.white,
+            fontFamily: fontFamily,
+            fontSize: 20,
+            fontStyle: FontStyle.italic),
+        body2:
+            base.body2.copyWith(color: Colors.white, fontFamily: fontFamily));
   }
+
   final ThemeData base = ThemeData.light();
   return base.copyWith(
       textTheme: _basicTextTheme(base.textTheme),
