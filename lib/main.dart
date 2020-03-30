5a2269e576c54f0b2bae334e37d3953842042c52
= <=>
2020-03-31 12:45:18 +1300

    VERSION 1.0!

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyStrengths extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: basicTheme(),
-      // debugShowCheckedModeBanner: false  ,
+      debugShowCheckedModeBanner: false  ,
       home: MyStrenghtsList(),
     );
   }
